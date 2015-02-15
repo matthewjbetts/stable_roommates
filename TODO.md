42d6e354db304485c527eebc0328e1e1b25747bf
Matthew Betts <bq_mbetts@bq-pc-rr10.bioquant.uni-heidelberg.de>
2015-02-15 11:03:19 +0100

    Source code now under GNU General Public License

--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
+* output pairs in one direction only
 * if the n_pairs >= n_pairs_max, then everyone can be paired with everyone else
 * allow for multiple rounds, not just one set of pairings
 * need to make sure that at each date round, all pairs are stable
--- lib/Matching/StableRoommates.pm
+++ lib/Matching/StableRoommates.pm
@@ -6,6 +6,21 @@
 
  http://en.wikipedia.org/wiki/Stable_roommates_problem
 
+ Copyright 2014, Matthew Betts
+
+ StableRoommates is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 =head1 EXAMPLE USAGE
 
 =head1 AUTHOR - Matthew Betts
